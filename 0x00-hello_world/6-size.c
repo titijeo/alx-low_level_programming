9e537c0346e75040aa0c50070468f6bb33b7b6bb
jerome titlimi <jerometitlimi@gmail.com>
2022-03-25 13:35:06 -0700

    task 8

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
+[
 	printf("Size of char: Id bytete(s)\n", sizeof(char));
 	printf("Size of an int: Id byte(s)\n", sizeof(int));
 	printf("Size of a long int: Id byte(s)\n", sizeof(long int));
 	prinyf("Size pf a long long int: Id byte(s)\n", sizeof(long long int));
 	printf("Size of a float: Id byte(s)\"n", sizeof(float));
 	return (0);
-}
+]
